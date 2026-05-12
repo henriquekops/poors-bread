4902a40827560e8f1215ff1fac7ae69950510f13
Henrique Kops <37592752+henriquekops@users.noreply.github.com>
2026-05-12 20:47:12 -0300

    add: crazy dash (2)

--- dashboard/main.py
+++ dashboard/main.py
@@ -5,10 +5,36 @@ import streamlit as st
 
 output_path = f"{Path(__file__).parent.parent}/data/output"
 
-df = pd.read_csv(f"{output_path}/processed.csv")
-df = df[df["Category"] == "Atendimentos indvidual"].sort_values(["Month"])
-df = df[["Month", "Value", "Category"]].head()
+month_order = {
+    m: i
+    for i, m in enumerate(
+        [
+            "JAN",
+            "FEV",
+            "MAR",
+            "ABR",
+            "MAI",
+            "JUN",
+            "JUL",
+            "AGO",
+            "SET",
+            "OUT",
+            "NOV",
+            "DEZ",
+        ]
+    )
+}
 
+df = pd.read_csv(f"{output_path}/processed.csv")
+
+df = df[df["Year"] == 2021]
+df["MonthIdx"] = df["Month"].map(month_order)
+df = df[["Month", "Value", "Category", "MonthIdx"]].sort_values(
+    ["MonthIdx"], ascending=True
+)
 
 st.title("Poor's Bread")
-st.line_chart(df, x="Month", y="Value", color="Category")
+
+st.line_chart(
+    df, x="MonthIdx", y="Value", x_label="Mes", y_label="Contagem", color="Category"
+)
